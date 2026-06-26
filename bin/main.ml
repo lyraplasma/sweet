522751da2d9a660bf1bcc1c70378f16bedb84fce
lyraplasma <274751584+lyraplasma@users.noreply.github.com>
2026-06-26 15:15:05 +0000

    chore:3 prettify

--- bin/main.ml
+++ bin/main.ml
@@ -1,13 +1,13 @@
 let () =
   let info = Info.collect () in
-  Printf.printf "Your Machine's Status :3";
-  Printf.printf "OS:        %s\n" info.Info.os;
-  Printf.printf "Hostname:  %s\n" info.Info.hostname;
-  Printf.printf "Kernel:    %s\n" info.Info.kernel;
-  Printf.printf "Uptime:    %s\n" info.Info.uptime;
-  Printf.printf "Packages:  %s\n" info.Info.packages;
-  Printf.printf "Shell:     %s\n" info.Info.shell;
-  Printf.printf "CPU:       %s\n" info.Info.cpu;
-  Printf.printf "GPU:       %s\n" info.Info.gpu;
-  Printf.printf "Memory:    %s\n" info.Info.memory;
-  Printf.printf "Disk:      %s\n" info.Info.disk
+  Printf.printf "<><><><><><> Your Machine's Status :3 <><><><><><>\n";
+  Printf.printf "\t\tOS:        %s\n" info.Info.os;
+  Printf.printf "\t\tHostname:  %s\n" info.Info.hostname;
+  Printf.printf "\t\tKernel:    %s\n" info.Info.kernel;
+  Printf.printf "\t\tUptime:    %s\n" info.Info.uptime;
+  Printf.printf "\t\tPackages:  %s\n" info.Info.packages;
+  Printf.printf "\t\tShell:     %s\n" info.Info.shell;
+  Printf.printf "\t\tCPU:       %s\n" info.Info.cpu;
+  Printf.printf "\t\tGPU:       %s\n" info.Info.gpu;
+  Printf.printf "\t\tMemory:    %s\n" info.Info.memory;
+  Printf.printf "\t\tDisk:      %s\n" info.Info.disk
